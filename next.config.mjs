7ca6b974078858bce3f7dcbb98f40f9626444db4
Noah Reed <Nore5515@gmail.com>
2025-02-09 18:46:42 -0800

    at last got a static build with css\. stupid fonts.

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,13 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
     // output: "standalone",
     // reactStrictMode: true,
+    // experimental: {
+    //     turbo: false
+    // }
+    output: 'export',
+    assetPrefix: './'
 };
 
 export default nextConfig;
